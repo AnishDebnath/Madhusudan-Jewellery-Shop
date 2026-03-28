0d24a5c0f2e3e7fe95c8542e93e5041d1ce3f9b3
Anish Debnath <118295754+AnishDebnath@users.noreply.github.com>
2026-03-28 05:46:49 +0530

    Switch to BrowserRouter for clean domain.com/page URLs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,13 +9,13 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
